acd738075be24d94e04b152561457dc8c474ad7d
nancyhawa <hawa.nancy@gmail.com>
2015-08-11 15:42:48 -0400

    many many changes

--- parks_location_data.rb
+++ parks_location_data.rb
@@ -3,18 +3,109 @@ require 'open-uri'
 require 'geocoder'
 require 'rest_client'
 require_relative 'parks_list'
+require_relative 'key'
+require 'pstore'
+require 'pry'
 class FoodCarts
 
   def initialize
     @data = 'http://www.nycgovparks.org/bigapps/DPR_Eateries_001.xml'
     @food_carts_api = Nokogiri::XML(open(@data).read)
+    @store = PStore.new("review_data.pstore")
+    # Geocoder.configure(:lookup => :bing, :api_key => bing_key)
   end
 
   def run
-    @park = closest_park(get_user_location)
-    puts "The closest park to you is #{park}."
-    puts "Here are the locations of the food carts in #{park}"
+    puts "Welcome to Food Cart Finder!!!"
+    main_menu
   end
+
+  def find_cart
+    puts "Would you like to tell us the name of your park, or have us determine the name of the closest park based on your location?  Enter '1' to give us a park, enter '2' to have us find a park near you."
+    @input = gets.chomp
+    case @input
+    when '1'
+      puts "Which park would you like to search?"
+      @answer = gets.chomp
+      respond_to_park_choice
+      print_food_cart_info
+    when '2'
+      @park = closest_park(get_user_location)
+      print_food_cart_info
+    end
+    request_review
+  end
+
+  def main_menu
+    puts "Would you like to FIND a cart or write a REVIEW?"
+    get_user_input
+    case @input.downcase
+    when 'find'
+      find_cart
+    when 'review'
+      write_review
+    else
+      puts "I didn't understand that."
+      main_menu
+    end
+
+  def request_review
+    puts "Would you like to add a review? Answer 'y' or 'n'."
+    get_user_input
+    write_review
+  end
+
+  def write_review
+    if @input.downcase == 'y'
+      puts "Which cart would you like to review.  Select the cart by number."
+      @index = get_user_input.to_i - 1
+      location = near_by_food_carts(@park)[@index]
+      puts "You selected #{location} in #{@park}."
+      puts "Rate the cart from 1-5."
+      rating = gets.chomp
+      puts "Would you like to add comments?"
+      response = gets.chomp
+      if response == 'y'
+        puts "Ok, input your comments."
+        comments = gets.chomp
+      else
+        puts "Ok, great."
+      end
+    elsif @input == 'n'
+      main_menu
+    else @input
+      puts "I didn't understand that answer."
+      request_review
+    else
+      # abort(Goodbye!)
+    end
+    binding.pry
+    store_review(location, rating, comments)
+  end
+
+  def get_user_input
+    @input = gets.chomp
+  end
+
+  def print_food_cart_info
+    puts "The closest park to you is #{@park}."
+    puts "Here are the locations of the food carts in #{@park}:"
+    near_by_food_carts(@park).each.with_index(1) do  |cart, i|
+      puts "#{i}. #{cart}.  RATING:#{get_rating(cart)}, #{get_comments(cart)}"
+    end
+  end
+
+  def respond_to_park_choice
+    if park_list_hash.has_key?(@answer)
+      @park = @answer
+      puts "The closest park to you is #{@park}."
+      puts "Here are the locations of the food carts in #{@park}:"
+      puts near_by_food_carts(@park)
+    else
+      puts "I'm sorry.  That park is not in our database."
+    end
+  end
+
 
   def park_names
     parks = []
@@ -24,6 +115,16 @@ class FoodCarts
     parks.uniq
   end
 
+  def park_locations_hash
+    hash = {}
+
+    parks_with_locations.each do |park|
+      hash[park] = park_coordinates(park)
+      sleep 1
+    end
+    hash
+
+  end
 
   # def parks_without_locations
   #   park_names.select { |park| !Geocoder.search(park)[0] }  # ~> NameError: undefined local variable or method `parks_names' for #<FoodCarts:0x007ff8342320d8>
@@ -31,10 +132,16 @@ class FoodCarts
 
   def park_coordinates(park)
     result = Geocoder.search(park)[0]
-    latitude = result.latitude
-    longitude = result.longitude
-    if !latitude || !longitude
-      return "There is no location data for this #{park}"
+    if result
+      latitude = result.latitude
+    else
+      latitude = 1000000000000000000
+    end
+
+    if result
+      longitude = result.longitude
+    else
+      longitude = 1000000000000000000
     end
     [latitude, longitude]
    end
@@ -43,9 +150,9 @@ class FoodCarts
     min_distance = nil
     closest_park = nil
 
-    parks_with_locations.each do | park |
-      lat_diff = (park_coordinates(park)[0] - user_coordinates[0]).abs
-      long_diff = (park_coordinates(park)[1] - user_coordinates[1]).abs
+    park_list_hash.each do | park, coordinates |
+      lat_diff = (coordinates[0] - user_coordinates[0]).abs
+      long_diff = (coordinates[1] - user_coordinates[1]).abs
       distance = (lat_diff**2 + long_diff**2)**(0.5)
       if !min_distance || min_distance > distance
         min_distance = distance
@@ -55,10 +162,10 @@ class FoodCarts
     closest_park
   end
 
-  def food_carts_in_closest_park(park)
+  def near_by_food_carts(park)
     nearby_carts = []
     @food_carts_api.css('facility').each do |cart|
-      nearby_carts << cart.css('location') if cart.css('name').include?(park)
+      nearby_carts << cart.css('location').text if cart.css('name').text.include?(park)
     end
     nearby_carts
   end
@@ -73,8 +180,30 @@ class FoodCarts
   end
 
 end
+
+#<---------------REVIEW_METHODS ----------------------->
+
+def store_review(location,rating,comment="NO COMMENTS")
+  @store.transaction do
+    @store[location] = {rating: rating, comment: comment}
+  end
+end
+
+def get_rating(location)
+   @store.transaction {@store[location][:rating]}
+   rescue
+   return "Unrated"
+ end
+
+def get_comments(location)
+  @store.transaction {@store[location][:comment]}
+  rescue
+  return "No comments"
+end
 # print ["Happy", "sad", "angry"]
 # print FoodCarts.new.parks_with_locations.length
 # FoodCarts.new.park_names.size
 
-FoodCarts.new.run
+ # print FoodCarts.new.park_locations_hash.select {|k,v| v != [1000000000000000000, 1000000000000000000] && !hash2.has_key?(k)}
+
+ FoodCarts.new.run
